fde963eee672d0d8a60d86e44dfcff62b553d625
Kiv Aujla <89060170+kaujla1@users.noreply.github.com>
2023-06-27 09:29:28 -0400

    Add basic maths functions

--- script.js
+++ script.js
@@ -1,1 +1,53 @@
+//DO NOT USE eval() or return a new Function()
+
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function divide(num1, num2) {
+  if (num2 === 0) {
+    return "Cannot divide by 0";
+  } else {
+    return num1 / num2; 
+  }
+}
+
+function power(num1, num2) {
+  return num1 ** num2;
+}
+
+//For divide, anything / 0 returns infinity, but should it return "can't divide by 0"? 
+
+
+let n1; 
+let n2; 
+let op; 
+
+function operate(num1, num2, operator) {
+  switch (operator) {
+    case "+":
+      return add(num1, num2);
+      break;
+    case "-":
+      return subtract(num1, num2);
+      breakl
+    case "*":
+      return multiply(num1, num2);
+      break;
+    case "/":
+      return divide(num1, num2);
+      break;
+    case "**":
+      return power(num1, num2);
+      break;
+  };
+}
 
