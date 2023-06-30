4628427a2e78657fc09a7534ae1966be395ac00c
Kiv Aujla <89060170+kaujla1@users.noreply.github.com>
2023-06-30 10:09:45 -0400

    Fix delete functionality on calculator

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-//DO NOT USE eval() or return a new Function()
-
 function add(num1, num2) {
   return num1 + num2;
 }
@@ -24,10 +22,10 @@ function power(num1, num2) {
   return num1 ** num2;
 }
 
-let prev = []; //types are number1, number2, op, equals
+let prev = []; 
 let prevLast; //might need to empty this on clear? Value is typeof string btw. Maybe change to prevLastType???
-let number1 = ""; //these might need to be arrays or objects?
-let number2 = ""; //instead of making these 0 to begin, just use +number1/2 when passing them to operate 
+let number1 = ""; 
+let number2 = ""; 
 let operator = ""; 
 
 function operate(num1, num2, op) {
@@ -87,7 +85,7 @@ function clearAll() {
 
 function displayValue(chosen) {
   setPrev();
-  
+  //add case for if prev === "/" && chosen === "0" return error? 
   if (chosen === "Clear") {
     clearAll();
   } else if (chosen === "Del") {
@@ -95,7 +93,7 @@ function displayValue(chosen) {
       ""; //should do nothing?
     } else if (prevLast === "number1") {
       number1 = number1.substring(0, (number1.length - 1));// put in a helper function?
-      lowerScreen.textContent.substring(0, (lowerScreen.textContent.length - 1));
+      lowerScreen.textContent = number1;
       prev.pop();
     } else if (prevLast === "op") {
       upperScreen.textContent = "";
@@ -104,11 +102,12 @@ function displayValue(chosen) {
       prev.pop();
     } else if (prevLast === "number2") {
       number2 = number2.substring(0, (number2.length - 1));// put in a helper function?
-      lowerScreen.textContent.substring(0, (lowerScreen.textContent.length - 1));
+      lowerScreen.textContent = number2;
       prev.pop();
     } else if (prevLast === "equals") {
       upperScreen.textContent = "";
-      lowerScreen.textContent.substring(0, (lowerScreen.textContent.length - 1));
+      number1 = number1.substring(0, (number1.length - 1));// put in a helper function?
+      lowerScreen.textContent = number1;
       prev.splice(-2, 2);
     }
   } else if (chosen === "=") {
@@ -120,7 +119,7 @@ function displayValue(chosen) {
       prev.push({choice: chosen, type: "equals"}); //redo
     } else if (prevLast === "number2") {
       upperScreen.textContent = `${number1} ${operator} ${number2} =`;
-      number1 = operate(number1, number2, operator);
+      number1 = operate(number1, number2, operator).toString();
       lowerScreen.textContent = number1;
       number2 = "";
       operator = "";
@@ -138,10 +137,11 @@ function displayValue(chosen) {
       upperScreen.textContent = `${number1} ${operator}`;
       prev.push({choice: chosen, type: "op"}); //redo
     } else if (prevLast === "number2") {
-      number1 = operate(number1, number2, operator); 
+      number1 = operate(number1, number2, operator).toString(); 
       operator = chosen;
       lowerScreen.textContent = "";
       upperScreen.textContent = `${number1} ${operator}`;
+      number2 = "";
       prev = []; //this might be redundant, consider removing
       addManyToPrev(number1, chosen);     
     } 
@@ -174,7 +174,7 @@ function addManyToPrev(num, op) {
       prev.push({choice: lastLetter, type: "number1"});
     }
     prev.push({choice: op, type: "equals"});
-  } else {
+  } else if (operators.includes(op)) {
     for (let i = 0; i < num.length; i++) {
       lastLetter = number1.slice(i, i+1);
       prev.push({choice: lastLetter, type: "number1"});
@@ -205,86 +205,3 @@ return num1+num2;
 }
 
 prev = [{choice: "1", type: "number1"}, {choice: "+", type: "op"}, {choice: "2", type: "number2"}];*/
-
-
-/*
-const screenContainer = document.querySelector(".screen-container");
-
-const buttons = document.querySelectorAll(".button-container button");
-
-const upperScreen = document.createElement("div");
-const lowerScreen = document.createElement("div");
-
-screenContainer.appendChild(upperScreen);
-screenContainer.appendChild(lowerScreen);
-
-function validateClick(choice) {
-  if (upperScreen.textContent === "" && !classes.includes("num"))
-}
-
-function assignValue() {
-  if (upperScreen.textContent === "") {
-    number1 = 
-  }
-}
-
-function displayValue() {
-  console.log("hi");
-}
-
-let clicked;
-let classes;
-
-buttons.forEach((button) => {
-  button.addEventListener("click", () => {
-    clicked = button.textContent;
-    classes = button.className;
-  });
-});*/
-
-/*function displayValue(chosen) {
-  if (chosen is num) {
-    if (operator is empty) {
-      number1 = number1+ chosen and prev = chosen, then show on lowerScreen
-    } else {
-      number2 = number2+ chosen and prev = chosen, then show on lowerScreen
-    }
-  } else if (chosen is op) {
-    if (upperScreen is empty) { //maybe change to if number1 is empty
-      error
-    } else if (prev is operator){
-      error
-    } else if (number2 isnt empty) {
-      call operator with number1, operator, and number2, then let number1 = result and operator = chosen, then clear prev THEN prev = result (separated digits) AND chosen, then show number1 and chosen on upperScreen, then clear lowerScreen
-    } else {
-      number1 followed by operator on upperScreen, and lowerScreen is empty, and operator = op, and prev = chosen
-    }
-  } else if (chosen is equals) {
-    if (upperScreen is empty) { //maybe change to if number1 is empty
-      error
-    } else if (prev is operator) {
-      error
-    } else if (previous is equals) {
-      do nothing
-    } else if (number1 isnt empty and number2 IS empty) {
-      number1 on upperScreen and on lowerScreen, prev = equals
-    } else {
-      call operator with number1, operator, and number2, then show number1 operator number2 equals on upperScreen and result on lowerScreen, then let number1 = results and prev = number1 (separated digits) AND equals, then clear number2 and operator
-    } 
-  } else if (chosen is clear) {
-    clear all screens, number1, number2, prev, and operator
-  } else if (chosen is del) {
-    if (number1 is empty) {
-      do nothing
-    } else if (previous is equals) {
-      clear upperScreen, then remove last digit from number1, then remove last digit from lowerScreen, then remove last TWO digits from previous (to account for the = and the deleted number)
-    } else if (number2 isnt empty) {
-      remove last from number2, remove last from previous, remove last from lowerScreen
-    } else if (operator isnt empty) {
-      clear operator, remove last from previous, remove last from upperScreen
-    } else {
-      remove last from number1, remove last from previous, remove last from lowerScreen
-    }
-  };
-}
-*/
