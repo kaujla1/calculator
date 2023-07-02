a26d6b7fdbf6aac4fdf8844938d9f07d9d2fed41
Kiv Aujla <89060170+kaujla1@users.noreply.github.com>
2023-07-02 10:07:54 -0400

    Refactor validateChoice using multiple helper functions

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Math operation functions
 function add(num1, num2) {
   return num1 + num2;
 }
@@ -45,7 +46,7 @@ function operate(num1, num2, op) {
   };
 }
 
-//Create the functions that populate the display when you click the number buttons. You should be storing the ‘display value’ in a variable somewhere for use in the next step.
+//Connect to and create calculator screen
 const upperScreenContainer = document.querySelector(".upper-screen-container");
 const lowerScreenContainer = document.querySelector(".lower-screen-container");
 
@@ -59,9 +60,26 @@ lowerScreen.classList.add(".lower");
 upperScreenContainer.appendChild(upperScreen);
 lowerScreenContainer.appendChild(lowerScreen);
 
-
+//Operate calculator
 let operators = ["^", "/", "x", "-", "+"];
 let numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+function validateChoice(chosen) {
+  setPrev();
+  if (chosen === "C") {
+    clearAll();
+  } else if (chosen === "Del") {
+    validateDelete();
+  } else if (chosen === "=") {
+    validateEquals(chosen);
+  } else if (operators.includes(chosen)) {
+    validateOperator(chosen);
+  } else if (chosen === ".") {
+    validatePeriod(chosen);
+  } else if (numbers.includes(chosen)) {
+    validateNumber(chosen);
+  }
+}
 
 function setPrev() {
   if (prev.length === 0) {
@@ -81,109 +99,133 @@ function clearAll() {
   upperScreen.textContent = "";
 }
 
-function displayValue(chosen) {
-  setPrev();
-  //add case for if prev === "/" && chosen === "0" return error? 
-  if (chosen === "C") {
-    clearAll();
-  } else if (chosen === "Del") {
-    if (number1.length === 0) {
-      ""; //should do nothing?
-    } else if (prevType === "number1" || prevType === "period1") {
-      number1 = number1.substring(0, (number1.length - 1));// put in a helper function?
-      lowerScreen.textContent = number1;
-      prev.pop();
-    } else if (prevType === "op") {
-      upperScreen.textContent = "";
-      lowerScreen.textContent = number1;
-      operator = "";
-      prev.pop();
-    } else if (prevType === "number2" || prevType === "period2") {
-      number2 = number2.substring(0, (number2.length - 1));// put in a helper function?
-      lowerScreen.textContent = number2;
-      prev.pop();
-    } else if (prevType === "equals") {
-      upperScreen.textContent = "";
-      number1 = number1.substring(0, (number1.length - 1));// put in a helper function?
-      lowerScreen.textContent = number1;
-      prev.splice(-2, 2);
-    }
-  } else if (chosen === "=") {
-    if (number1.length === 0 || prevType === "op") {
-      lowerScreen.textContent = "Error";
-    } else if (prevType === "number1") {
-      upperScreen.textContent = `${number1} =`;
-      lowerScreen.textContent = number1;
-      prev.push({choice: chosen, type: "equals"}); //redo
-    } else if (prevType === "number2") {
-      upperScreen.textContent = `${number1} ${operator} ${number2} =`;
-      number1 = operate(number1, number2, operator).toString();
-      lowerScreen.textContent = number1;
-      number2 = "";
-      operator = "";
-      prev = []; //not sure if this is redundant, consider removing
-      addManyToPrev(number1, chosen)  
-    } else if (prevType === "equals") {
-      "";
-    }
-  } else if (operators.includes(chosen)) {
-    if (number1.length === 0 || prevType === "op") {
-      lowerScreen.textContent = "Error";
-    } else if (prevType === "number1" || prevType === "equals") { //not sure if merging these will work
-      operator = chosen;
-      lowerScreen.textContent = "";
-      upperScreen.textContent = `${number1} ${operator}`;
-      prev.push({choice: chosen, type: "op"}); //redo
-    } else if (prevType === "number2") {
-      number1 = operate(number1, number2, operator).toString(); 
-      operator = chosen;
-      lowerScreen.textContent = "";
-      upperScreen.textContent = `${number1} ${operator}`;
-      number2 = "";
-      prev = []; //this might be redundant, consider removing
-      addManyToPrev(number1, chosen);     
-    } 
-  } else if (chosen === ".") {
-    if (number1.length === 0) {
-      number1 = "0.";
-      lowerScreen.textContent = number1;
-      prev.push({choice: "0", type: "number1"});
-      prev.push({choice: chosen, type: "period1"});
-    } else if (prevType === "number1" && !number1.includes(".")) {
-      number1 = number1 + chosen; 
-      lowerScreen.textContent = number1;
-      prev.push({choice: chosen, type: "period1"});
-    } else if (prevType === "op") {
-      number2 = "0.";
-      lowerScreen.textContent = number2;
-      prev.push({choice: "0", type: "number2"});
-      prev.push({choice: chosen, type: "period2"});
-    } else if (prevType === "number2" && !number2.includes(".")) {
-      number2 = number2 + chosen;
-      lowerScreen.textContent = number2;
-      prev.push({choice: chosen, type: "period2"});
-    }
-  } else if (numbers.includes(chosen)) {
-    if (number1.length === 0) {
-      number1 = chosen;
-      lowerScreen.textContent = number1;
-      prev.push({choice: chosen, type: "number1"}); //redo
-    } else if (prevType === "number1" || prevType === "equals" || prevType === "period1") { 
-      number1 = number1 + chosen; 
-      lowerScreen.textContent = number1;
-      prev.push({choice: chosen, type: "number1"}); //redo
-    } else if (chosen === "0" && prevChoice === "/") {
-      lowerScreen.textContent = "Cannot divide by zero";
-    } else if (prevType === "op") {
-      number2 = chosen;
-      lowerScreen.textContent = number2;
-      prev.push({choice: chosen, type: "number2"}); //redo
-    } else if (prevType === "number2" || prevType === "period2") {
-      number2 = number2 + chosen;
-      lowerScreen.textContent = number2;
-      prev.push({choice: chosen, type: "number2"});
-    } 
-  };
+function validateDelete() {
+  if (number1.length === 0) {
+    ""; //should do nothing?
+  } else if (prevType === "number1" || prevType === "period1") {
+    // number1 = number1.substring(0, (number1.length - 1));
+    // lowerScreen.textContent = number1;
+    deleteLetter(number1);
+    prev.pop(); //add to helper
+  } else if (prevType === "op") {
+    upperScreen.textContent = "";
+    lowerScreen.textContent = number1;
+    operator = "";
+    prev.pop(); //add to helper
+  } else if (prevType === "number2" || prevType === "period2") {
+    // number2 = number2.substring(0, (number2.length - 1));
+    // lowerScreen.textContent = number2;
+    deleteLetter(number2);
+    prev.pop();
+  } else if (prevType === "equals") {
+    upperScreen.textContent = "";
+    // number1 = number1.substring(0, (number1.length - 1));
+    // lowerScreen.textContent = number1;
+    deleteLetter(number1);
+    prev.splice(-2, 2); //add to helper
+  }
+}
+
+function deleteLetter(number) {
+  if (number === number1) {
+    number1 = number1.substring(0, (number1.length - 1));
+    lowerScreen.textContent = number1;
+  } else if (number === number2) {
+    number2 = number2.substring(0, (number2.length - 1));
+    lowerScreen.textContent = number2;
+  }
+
+}
+
+function validateEquals(chosen) {
+  if (number1.length === 0 || prevType === "op") {
+    lowerScreen.textContent = "Error";
+  } else if (prevType === "number1") {
+    upperScreen.textContent = `${number1} =`;
+    lowerScreen.textContent = number1;
+    prev.push({choice: chosen, type: "equals"}); //redo
+  } else if (prevType === "number2") {
+    upperScreen.textContent = `${number1} ${operator} ${number2} =`;
+    number1 = operate(number1, number2, operator).toString();
+    lowerScreen.textContent = number1;
+    number2 = "";
+    operator = "";
+    prev = []; //not sure if this is redundant, consider removing
+    addManyToPrev(number1, chosen)  
+  } else if (prevType === "equals") {
+    "";
+  }
+}
+
+function validateOperator(chosen) {
+  if (number1.length === 0 || prevType === "op") {
+    lowerScreen.textContent = "Error";
+  } else if (prevType === "number1" || prevType === "equals") { 
+    operator = chosen;
+    lowerScreen.textContent = "";
+    upperScreen.textContent = `${number1} ${operator}`;
+    prev.push({choice: chosen, type: "op"}); //redo
+  } else if (prevType === "number2") {
+    number1 = operate(number1, number2, operator).toString(); 
+    operator = chosen;
+    lowerScreen.textContent = "";
+    upperScreen.textContent = `${number1} ${operator}`;
+    number2 = "";
+    prev = []; //this might be redundant, consider removing
+    addManyToPrev(number1, chosen);     
+  } 
+}
+
+function validatePeriod(chosen) {//add case for decimal before operator (currently does nothing) (e.g., 1. then op, does nothing)
+  if (number1.length === 0) {
+    number1 = "0.";
+    lowerScreen.textContent = number1;
+    prev.push({choice: "0", type: "number1"});
+    prev.push({choice: chosen, type: "period1"});
+  } else if (prevType === "number1" && !number1.includes(".")) {
+    number1 = number1 + chosen; 
+    lowerScreen.textContent = number1;
+    prev.push({choice: chosen, type: "period1"});
+  } else if (prevType === "op") {
+    number2 = "0.";
+    lowerScreen.textContent = number2;
+    prev.push({choice: "0", type: "number2"});
+    prev.push({choice: chosen, type: "period2"});
+  } else if (prevType === "number2" && !number2.includes(".")) {
+    number2 = number2 + chosen;
+    lowerScreen.textContent = number2;
+    prev.push({choice: chosen, type: "period2"});
+  }
+}
+
+function validateNumber(chosen) {
+  if (number1.length === 0) {
+    number1 = chosen;
+    lowerScreen.textContent = number1;
+    addToPrev(chosen, "number1");
+    // prev.push({choice: chosen, type: "number1"}); //redo
+  } else if (prevType === "number1" || prevType === "equals" || prevType === "period1") { 
+    number1 = number1 + chosen; 
+    lowerScreen.textContent = number1;
+    addToPrev(chosen, "number1");
+    // prev.push({choice: chosen, type: "number1"}); //redo
+  } else if (chosen === "0" && prevChoice === "/") {
+    lowerScreen.textContent = "Cannot divide by zero";
+  } else if (prevType === "op") {
+    number2 = chosen;
+    lowerScreen.textContent = number2;
+    addToPrev(chosen, "number2");
+    // prev.push({choice: chosen, type: "number2"}); //redo
+  } else if (prevType === "number2" || prevType === "period2") {
+    number2 = number2 + chosen;
+    lowerScreen.textContent = number2;
+    addToPrev(chosen, "number2");
+    // prev.push({choice: chosen, type: "number2"});
+  } 
+}
+
+function addToPrev(userChoice, choiceType) {
+  prev.push({choice: userChoice, type: choiceType})
 }
 
 function addManyToPrev(num, op) {
@@ -200,15 +242,16 @@ function addManyToPrev(num, op) {
       prev.push({choice: lastLetter, type: "number1"});
     }
     prev.push({choice: op, type: "op"});
-  }
+  } //add case here for decimals? 
 }
 
+//Connect calculator operation to calculator buttons
 let clicked; 
 
 buttons.forEach((button) => {
   button.addEventListener("click", () => {    
     clicked = button.textContent;
-    displayValue(clicked);
+    validateChoice(clicked);
   });
 });
 
